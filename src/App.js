8eb0bb1fd90366a2ce96e98493a2f6aae51dbd9a
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-07-05 22:06:28 +0700

    spliting counter annd canvas into components, As the canvas is about cursor pointer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import { useCounter } from './hook-components/useCounter';
-import { useState} from 'react';
-export default function Counter() {
-  const [delay, SetDelay] = useState(1000);
-  const count = useCounter();
-  return (
+import React from "react";
+import Canvas from "./Canvas";
+import Counter from "./Counter";
+
+export default function App(){
+  return(
     <>
-      <label>
-        Tick duration: {delay} ms
-        <br />
-        <input
-          type = "range"
-          value = {delay}
-          min= "10"
-          max="2000"
-          onChange={e => SetDelay(Number(e.target.value))}
-          />
-      </label>
-      <hr />
-        <h1>Ticks: {count}</h1>
+      <Canvas />
+      <Counter />
     </>
   );
 }
+
