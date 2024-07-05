e74ab6e1ba49224353ffa0dc01f90b00f4c397ce
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-07-05 15:15:08 +0700

    extract useCounter hook and configure delay as a counter parameter

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useCounter } from './hook-components/useCounter';
+import { useState} from 'react';
+export default function Counter() {
+  const [delay, SetDelay] = useState(1000);
+  const count = useCounter();
+  return (
+    <>
+      <label>
+        Tick duration: {delay} ms
+        <br />
+        <input
+          type = "range"
+          value = {delay}
+          min= "10"
+          max="2000"
+          onChange={e => SetDelay(Number(e.target.value))}
+          />
+      </label>
+      <hr />
+        <h1>Ticks: {count}</h1>
+    </>
+  );
+}
